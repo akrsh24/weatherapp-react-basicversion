16612d654f536d99b988b85547fb4a851fb7be36
ZoCodes <zoeblogg91@gmail.com>
2021-10-12 18:16:47 +0100

    Updated meta title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello world</header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/zoeelizabethblogg/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Zoe Blogg
+          </a>
+          and is
+          <a
+            href="https://github.com/ZoCodes/weatherapp-react-basicversion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://zocodes-react-basicweatherapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/WeatherIcon.js
@@ -0,0 +1,1 @@
+// try to link media images
